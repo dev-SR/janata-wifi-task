6b7352f15cffc0c5f51090007ccec473ccbe8e61
Soikat Rahman <hello.dev.sr@gmail.com>
2024-02-08 00:02:07 +0600

    task1-extra: pagination added

--- stockview/views.py
+++ stockview/views.py
@@ -7,4 +7,7 @@ def home(request):
     with open('./stockview/stock_market_data.json') as f:
         data = json.load(f)
 
-    return render(request, 'stockview/home.html', {'data': data[:10]})
+    paginator = Paginator(data, 10)  # Show 10 items per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'stockview/home.html', {'page_obj': page_obj})
